1fc15f09d75845c20150fbef1b56ca19a9e546d2
Jane <994824764@qq.com>
2023-06-15 17:09:52 +0800

    vue3+ts ref和reactive

---
+++ vue-ts-prepare/02-defineEmits/App.vue
@@ -0,0 +1,15 @@
+<script setup lang='ts'>
+import son from './son.vue'
+// 4. 通过事件接收参数
+const passv = (value: number) => {
+  console.log(value);
+  
+}
+</script>
+<template>
+  <h2>defineEmits</h2>
+  <h3>父组件</h3>
+  <!-- 3. 绑定事件 -->
+  <son @passv="passv"></son>
+</template>
+<style scoped></style>
---
+++ vue-ts-prepare/03-ref/App.vue
@@ -0,0 +1,42 @@
+<script setup lang='ts'>
+import { ref } from 'vue';
+// 1. ref的用法
+const num1 = ref(100)
+
+// 2. vue3+ts 中的 ref （通过加泛型 决定传入数据的类型）
+// 2.1 简单数据类型
+const num2 = ref<number>(100)
+const num3 = ref(100)
+
+
+// 2.2 复杂数据类型
+// 配合 ts 使用（复杂数据类型） - 对象
+type TObj = {
+  name: string
+  age: number
+  gender: '男' | '女'
+}
+const obj = ref<TObj>({} as TObj) // 需要类型断言
+// 从网络请求中来
+setTimeout(() => {
+  obj.value = {
+    name: 'jane',
+    age: 26,
+    gender: '女'
+  }
+}, 1000)
+
+// 配合 ts 使用（复杂数据类型）- 数组**
+const list = ref<TObj[]>([]) // 表示数组长度为0 不需要类型推断
+// 从网络请求中得到数据
+setTimeout(() => {
+  list.value = [
+    { name: 'a', age: 1, gender: '女' },
+    { name: 'b', age: 2, gender: '男' }
+  ]
+}, 1000)
+</script>
+<template>
+  <h2>ref的使用</h2>
+</template>
+<style scoped></style>
