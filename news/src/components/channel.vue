0cab02c48a19ea9370c27ed8512f8dc83d8131ba
Jane <994824764@qq.com>
2023-06-16 15:31:47 +0800

    频道高亮处理

--- news/src/components/channel.vue
+++ news/src/components/channel.vue
@@ -5,16 +5,18 @@ import { storeToRefs } from 'pinia';
 // 得到 channel 的 store 实例
 const channelStore = useChannelStore()
 // 调用方法得到数据源
-const {getData} = channelStore
+const {getData,changeActive} = channelStore
 onMounted(()=>{
     getData()
 })
 // 解构数据
-const {channelList} = storeToRefs(channelStore)
+const {channelList,active} = storeToRefs(channelStore)
 </script>
 <template>
   <ul class="catagtory">
-    <li v-for="item in channelList" :key="item.id" class="select">{{item.name}}</li>
+    <!-- <li class="select" v-for="item in channelList" :key="item.id">{{item.name}}</li> -->
+    <li @click="changeActive(item.id)" :class="{select: active === item.id}" v-for="item in channelList" :key="item.id">{{item.name}}</li>
+
   </ul>
 </template>
 <style scoped lang='less'></style>  
--- news/src/store/modules/channel.ts
+++ news/src/store/modules/channel.ts
@@ -16,16 +16,29 @@ type TRes = {
 }
 // 创建频道 store 的函数
 let useChannelStore = defineStore('channel', () => {
+    // ------ 数据源相关
     // 定义数据源
     const channelList = ref<Channel[]>([])
     // 定义获取数据源的方法
     const getData = async () => {
         const {data: res} = await axios.get<TRes>('http://geek.itheima.net/v1_0/channels')
-        channelList.value = res.data.channels    
+        channelList.value = res.data.channels 
+        // 当获取频道数据源之后，应该给 active 设置一个默认值 （第一个数据的id）
+        active.value = channelList.value[0].id  
+    }
+
+    // ------ 选中的频道相关
+    // 定义选中频道的 id
+    const active = ref(0)
+    // 定义切换频道的方法
+    const changeActive = (id: number) => {
+        active.value = id
     }
     return {
         channelList,
-        getData
+        getData,
+        active,
+        changeActive
     }
 })
 
