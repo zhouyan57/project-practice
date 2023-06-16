051afa486dd9b7f5ff1d179ea20f78b507ada3cf
Jane <994824764@qq.com>
2023-06-16 13:34:33 +0800

    搭建头条新闻项目结构

---
+++ news/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang='ts'>
+import Channel from './components/channel.vue'
+import NewsList from './components/newslist.vue'
+</script>
+<template>
+  <channel></channel>
+  <news-list></news-list>
+</template>
+<style scoped></style>
---
+++ news/src/components/newslist.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="list">
+    <div class="article_item">
+      <h3 class="van-ellipsis">python数据预处理 ：数据标准化</h3>
+      <div class="img_box">
+        <img src="../assets/logo.png" class="w100" />
+      </div>
+      <div class="info_box">
+        <span>13552285417</span>
+        <span>0评论</span>
+        <span>2018-11-29T17:02:09</span>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped lang="less"></style>
