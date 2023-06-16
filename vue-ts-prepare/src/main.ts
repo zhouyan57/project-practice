aad8386f1dc457baaa65e1f9e7a2e0a61064a525
Jane <994824764@qq.com>
2023-06-16 11:23:43 +0800

    给JS文件提供类型

--- vue-ts-prepare/src/main.ts
+++ vue-ts-prepare/src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import type { Channel, Res } from './type'
-
+import {add, baseurl} from './utils/tools.js'
+console.log(add(1,2))
+console.log(baseurl)
 
 createApp(App).mount('#app')
