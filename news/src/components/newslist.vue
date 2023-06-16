65e1e335e0cdbf61c52171930a2d0a2634b9d575
Jane <994824764@qq.com>
2023-06-16 17:03:18 +0800

    渲染文章数据

--- news/src/components/newslist.vue
+++ news/src/components/newslist.vue
@@ -1,17 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { useChannelStore, useNewsListStore } from '../store'
+import { watch } from 'vue';
+const newsListStore = useNewsListStore()
+const channelStore = useChannelStore()
+const { getArtList } = newsListStore
+const { active } = storeToRefs(channelStore)
+const { artList } = storeToRefs(newsListStore)
+// 侦听 active
+watch(active, () => {
+    getArtList(active.value)
+}, {
+    immediate: true
+})
+</script>
 <template>
-  <div class="list">
-    <div class="article_item">
-      <h3 class="van-ellipsis">python数据预处理 ：数据标准化</h3>
-      <div class="img_box">
-        <img src="../assets/logo.png" class="w100" />
-      </div>
-      <div class="info_box">
-        <span>13552285417</span>
-        <span>0评论</span>
-        <span>2018-11-29T17:02:09</span>
-      </div>
+    <div class="list">
+        <div class="article_item" v-for="item in artList" :key="item.art_id">
+            <h3 class="van-ellipsis">{{ item.title }}</h3>
+            <div class="img_box" v-if="item.cover.type > 0">
+                <img src="item.cover.images[0]" class="w100" />
+            </div>
+            <div class="info_box">
+                <span>{{ item.aut_name }}</span>
+                <span>{{ item.comm_count }}评论</span>
+                <span>{{ item.pubdate }}</span>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 <style scoped lang="less"></style>
--- news/src/store/modules/newslist.ts
+++ news/src/store/modules/newslist.ts
@@ -1,6 +1,39 @@
 // 管理新闻的数据
+import axios from 'axios'
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
+// 定义类型
+type News = {
+    art_id: string
+    title: string
+    aut_id: string
+    comm_count: number
+    pubdate: string
+    aut_name: string
+    is_top: number
+    cover: {
+        type: number
+        images: string[]
+    }
+}
+type TRes = {
+    data: {
+        results: News[]
+        pre_timestamp: string
+    },
+    message: string
+}
 let useNewsListStore = defineStore('newsList', () => {
-    return {}
+    // 定义文章数据源
+    const artList = ref<News[]>([])
+    // 定义获取文章数据的方法
+    const getArtList = async (id: number) => {
+        const { data: res } = await axios.get<TRes>(`http://geek.itheima.net/v1_0/articles?channel_id=${id}&timestamp=${Date.now()}`)
+        artList.value = res.data.results
+    }
+    return {
+        artList,
+        getArtList
+    }
 })
-export { useNewsListStore}
+export { useNewsListStore }
