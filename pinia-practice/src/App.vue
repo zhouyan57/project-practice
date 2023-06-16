159c0443a4eceba50054fd442326ecbb8725cc67
Jane <994824764@qq.com>
2023-06-16 12:55:16 +0800

    pinia 在仓库中进行状态管理并使用

--- pinia-practice/src/App.vue
+++ pinia-practice/src/App.vue
@@ -1,6 +1,17 @@
 
-<script setup lang='ts'></script>
+<script setup lang='ts'>
+// 1. 导入获取的 store 函数
+import { useCounterStore } from './store/counter';
+// 2. 得到store对象
+const counter = useCounterStore()
+</script>
 <template>
   <h2>pinia用法</h2>
+  <h3>1. 使用 pinia 中的数据(state)</h3>
+  <div>count:{{ counter.count }}</div>
+  <h3>2. 使用 pinia 中的计算属性(getters)</h3>
+  <div>doubleCount:{{ counter.doubleCount }}</div>
+  <h3>3. 使用 pinia 中的方法(mutations/actions)</h3>
+  <button @click="counter.add">点我 count ++</button>
 </template>
 <style scoped></style>
--- pinia-practice/src/store/counter.ts
+++ pinia-practice/src/store/counter.ts
@@ -3,6 +3,19 @@
 //   defineStore：创建 pinia 仓库对象
 //   defineStore('仓库名', () => { return // 逻辑代码 }) // return 暴露给外界使用的数据和方法
 import {defineStore} from 'pinia'
-export const useCounterStore = defineStore('counter', ()=>{
-    return {}
+import { computed, ref } from 'vue'
+export const useCounterStore = defineStore('counter', () => {
+    // 定义变量保存数字 （相当于 vuex 中的 state）
+    const count = ref(1)
+
+    // 定义计算属性保存数字的两倍 （相对于 vuex 中的 getters）
+    const doubleCount = computed(()=>{
+        return count.value * 2
+    })
+
+    // 定义修改数字的方法 （相当于 vuex 中的 mutations & actions）
+    const add = () => {
+        count.value ++
+    }
+    return {count, add, doubleCount}
 })
