75d194b231eb98853749653a207655e27543cf80
Jane <994824764@qq.com>
2023-06-16 13:04:00 +0800

    storeToRefs的使用

--- pinia-practice/src/App.vue
+++ pinia-practice/src/App.vue
@@ -1,9 +1,15 @@
 
 <script setup lang='ts'>
 // 1. 导入获取的 store 函数
+import { storeToRefs } from 'pinia';
 import { useCounterStore } from './store/counter';
 // 2. 得到store对象
 const counter = useCounterStore()
+// 3. 将 store 中的数据解构出来 (storeToRefs: 解构数据时保留响应式)
+// const {count} = counter
+const {count,doubleCount} = storeToRefs(counter)
+// 解构出来的方法不需要
+const {add} = counter
 </script>
 <template>
   <h2>pinia用法</h2>
@@ -13,5 +19,10 @@ const counter = useCounterStore()
   <div>doubleCount:{{ counter.doubleCount }}</div>
   <h3>3. 使用 pinia 中的方法(mutations/actions)</h3>
   <button @click="counter.add">点我 count ++</button>
+  <h3>4. 使用解构出来的 count doubleCount</h3>
+  <div>count: {{ count }}</div>
+  <div>doubleCount: {{ doubleCount }}</div>
+  <h3>5. 解构出来的 add 方法</h3>
+  <button @click="add">点我 count ++</button>
 </template>
 <style scoped></style>
